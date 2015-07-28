2453b9bbe5f8da75b9b6dbe3f6d972fdfca0603b
roopesh90 <roopesh90nair@live.com>
2015-07-28 16:35:55 +0530

    urls cleanup

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -8,14 +8,11 @@ from rest_auth.views import (
 urlpatterns = patterns(
     '',
     # URLs that do not require a session or valid token
-    url(r'^password/reset/$', PasswordReset.as_view(),
-        name='rest_password_reset'),
-    url(r'^password/reset/confirm/$', PasswordResetConfirm.as_view(),
-        name='rest_password_reset_confirm'),
+    url(r'^password/reset/$', PasswordReset.as_view(), name='rest_password_reset'),
+    url(r'^password/reset/confirm/$', PasswordResetConfirm.as_view(), name='rest_password_reset_confirm'),
     url(r'^login/$', Login.as_view(), name='rest_login'),
     # URLs that require a user to be logged in with a valid session / token.
     url(r'^logout/$', Logout.as_view(), name='rest_logout'),
     url(r'^user/$', UserDetails.as_view(), name='rest_user_details'),
-    url(r'^password/change/$', PasswordChange.as_view(),
-        name='rest_password_change'),
+    url(r'^password/change/$', PasswordChange.as_view(), name='rest_password_change'),
 )
